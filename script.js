81d04d1ad27456fe4254a8a6f6b40b30fc1cede1
AlexMqs <alexandremarques73000@gmail.com>
2024-04-03 20:27:07 +0200

    Ajout d'un prompt avec un premier mot et une reponse console

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const motApplication = "Test" 
+let motUtilisateur = prompt("Entrez le mot : " + motApplication)
+
+if (motUtilisateur === motApplication) {
+    console.log("Bravo !")
+} else {
+    console.log("Vous avez fait une erreur de frappe.")
+}
